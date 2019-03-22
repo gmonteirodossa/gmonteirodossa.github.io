0b23abe95e2dfce5b9e56d5d55cca28c80023473
gmonteirodossa <43502643+gmonteirodossa@users.noreply.github.com>
2019-03-22 01:41:53 -0400

    Added

---
+++ contact-form-handler.php
@@ -0,0 +1,25 @@
+<?php
+  
+  /*$todo = $_POST['name'];
+  $j = $_GET['i'];*/
+  $email_to = $_POST['email'];
+  $email_from = "gabrielmonteiro.g10@gmail.com";
+  $email_subject = "Your To-Do List";
+  $email_body = "Todo: $todo.\n";
+  $headers = "From: $email_from \r\n";
+  $headers .= "Reply-to: $email_to \r\n";
+  $content = "aaaaaa";
+  /*for( $i = 0; $i<j; $i++ ) {
+            $todo = $_POST['name' .i];
+            $content = $content . $todo;
+         }
+         */
+  if(mail($email_to,$email_subject,$content,$headers)){
+     echo("Email successfully sent to $email_to...");
+  }
+  else {
+      echo("Email sending failed...");
+   }
+  header("Location: index.html");
+       
+?>
